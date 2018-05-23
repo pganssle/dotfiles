35e237cbe2cc9e0abc5b13a6535669f2e2cd9741
Paul Ganssle <paul@ganssle.io>
2018-05-23 11:39:54 -0400

    Make sure that the ~/.vim directory exists independent of stow

--- install.sh
+++ install.sh
@@ -7,6 +7,11 @@ git
 vim
 )
 
+# Create ~/.vim so it doesn't end up as a symlink
+if [ ! -d "~/.vim" ]; then
+    mkdir ~/.vim
+fi
+
 for pkg in "${PACKAGES[@]}"; do
     stow -d dotfiles -t ~ ${pkg}
 done
