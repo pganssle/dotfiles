9b9fbd7f9a3f15ccd944be0a147ca6dd3b9d5d22
Paul Ganssle <paul@ganssle.io>
2019-10-07 10:16:18 -0400

    Add ipython configuration
    Because ipython itself generates a bunch of stuff in these directories,
    I'm actually stowing the "profile_default" directly into
    ~/.ipython/profile_default rather than creating an "ipython" package. We
    will see if this is desirable in the long run, I suppose.

---
+++ dotfiles/ipython_profile_default/ipython_config.py
@@ -0,0 +1,28 @@
+import platform
+import IPython.core.release
+
+IPYTHON_VERSION = IPython.core.release.version
+PYTHON_IMPLEMENTATION = platform.python_implementation()
+PYTHON_VERSION = platform.python_version()
+PYTHON_BUILD = platform.python_build()
+
+# Single line startup banner
+# Switch to f-string when 3.6 is an implausible version to use
+BANNER_TEMPLATE = (
+    "IPython {IPYTHON_VERSION}, on {PYTHON_IMPLEMENTATION} "
+    + "{PYTHON_VERSION} ({PYTHON_BUILD[0]}, {PYTHON_BUILD[1]})\n"
+)
+
+c.InteractiveShell.banner1 = BANNER_TEMPLATE.format(
+    IPYTHON_VERSION=IPYTHON_VERSION,
+    PYTHON_VERSION=PYTHON_VERSION,
+    PYTHON_BUILD=PYTHON_BUILD,
+    PYTHON_IMPLEMENTATION=PYTHON_IMPLEMENTATION,
+)
+
+# No separator  between output and next input prompt
+c.InteractiveShell.separate_in = ""
+c.TerminalInteractiveShell.confirm_exit = False
+
+# Classic python REPL prompts (e.g. >>>, ...)
+c.TerminalInteractiveShell.prompts_class = "IPython.terminal.prompts.ClassicPrompts"
--- install.sh
+++ install.sh
@@ -17,5 +17,7 @@ for pkg in "${PACKAGES[@]}"; do
     stow -d dotfiles -t ~ ${pkg}
 done
 
+stow -d dotfiles -t ~/.ipython/profile_default ipython_profile_default
+
 stow -t ~ scripts
 stow -t ~ config_files
